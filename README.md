e1d39ba6b7b1b5841277b443245b16cada76970c
Andermit81 <mitchell2002touchdown@gmail.com>
2025-01-27 22:09:01 +0000

    Pending changes exported from your codespace

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS2450---G2-Project
 Task Manager 
+
+The purpose of our program is to organize tasks in a task manager.
--- Task_Mananger_Application.py
+++ Task_Mananger_Application.py
@@ -1,1 +1,4 @@
 "Group Project - Task Manager Application"
+"This is where we will write the code for our Project"
+
+print("Hello World!")
