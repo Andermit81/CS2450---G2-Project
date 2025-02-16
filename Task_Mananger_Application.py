7991c2129105c85187428fd66583c1cb78d8d7dd
Noah-Blevins <noah.james.blevins@gmail.com>
2025-02-16 13:51:14 -0700

    Created Task and TaskManager tasks

--- Task_Mananger_Application.py
+++ Task_Mananger_Application.py
@@ -1,2 +1,17 @@
 "Group Project - Task Manager Application"
 print("hello world")
+
+class Task:
+    id = 0
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
+        self.id = Task.id
+        Task.id += 1
+        
+    def mark_done(self):
+        self.completed = True
+    
+class TaskList:
+    def __init__(self):
+        self.tasks = {}
