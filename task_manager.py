69bef28a05d03a1b6e3675a408c6545564d27397
Noah-Blevins <noah.james.blevins@gmail.com>
2025-02-20 23:22:30 -0700

    Fixed saveload and switch statements

--- task_manager.py
+++ task_manager.py
@@ -21,8 +21,9 @@ class TaskManager:
         
     def save_tasks(self):
         tasks_dict = {}
-        for task in self.tasks:
-            tasks_dict[task.task_id] = [task.title, task.description, task.due_date, task.priority]
+        for task_id in self.tasks:
+            target_task = self.tasks[task_id]
+            tasks_dict[task_id] = [target_task.title, target_task.description, target_task.due_date, target_task.priority]
         task_json = json.dumps(tasks_dict)
         with open("tasklist.json", 'w') as tasks_file:
             tasks_file.writelines(task_json)
@@ -31,16 +32,15 @@ class TaskManager:
     def load_tasks(self):
         try:
             with open("tasklist.json", 'r') as tasks_file:
-                task_json = tasks_file.readlines()
+                task_json = tasks_file.read()
                 tasks_dict = json.loads(task_json)
                 self.tasks = {}
                 for taskitem_id in tasks_dict.keys():
-                    added_task = Task()
+                    added_task = Task(tasks_dict[taskitem_id][0])
                     added_task.task_id = taskitem_id
-                    added_task.title = tasks_dict[taskitem_id[0]]
-                    added_task.description = tasks_dict[taskitem_id[1]]
-                    added_task.due_date = tasks_dict[taskitem_id[2]]
-                    added_task.priority = tasks_dict[taskitem_id[3]]
+                    added_task.description = tasks_dict[taskitem_id][1]
+                    added_task.due_date = tasks_dict[taskitem_id][2]
+                    added_task.priority = tasks_dict[taskitem_id][3]
                     self.tasks[taskitem_id] = added_task
             print("Tasks loaded successfully!")
         except:
