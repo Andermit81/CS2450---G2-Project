c54258c4cab421c8c6c2729b23b5392a950fcf74
CarsonDSGrant <139571239+CarsonDSGrant@users.noreply.github.com>
2025-03-31 22:42:11 -0600

    Update task_manager.py
    save/load methods no follow the Singleton pattern

--- task_manager.py
+++ task_manager.py
@@ -21,32 +21,10 @@ class TaskManager:
         del self.tasks[task_id]
         
     def save_tasks(self):
-        tasks_dict = {}
-        for task_id in self.tasks:
-            target_task = self.tasks[task_id]
-            tasks_dict[task_id] = [target_task.title, target_task.description, target_task.due_date, target_task.priority, target_task.tags]
-        task_json = json.dumps(tasks_dict)
-        with open("tasklist.json", 'w') as tasks_file:
-            tasks_file.writelines(task_json)
-        print("Tasks saved!")
-    
+        TaskStorage().save_tasks(self.tasks)  
+
     def load_tasks(self):
-        try:
-            with open("tasklist.json", 'r') as tasks_file:
-                task_json = tasks_file.read()
-                tasks_dict = json.loads(task_json)
-                self.tasks = {}
-                for taskitem_id in tasks_dict.keys():
-                    added_task = Task(tasks_dict[taskitem_id][0])
-                    added_task.task_id = taskitem_id
-                    added_task.description = tasks_dict[taskitem_id][1]
-                    added_task.due_date = tasks_dict[taskitem_id][2]
-                    added_task.priority = tasks_dict[taskitem_id][3]
-                    added_task.tags = tasks_dict[taskitem_id][4]
-                    self.tasks[taskitem_id] = added_task
-            print("Tasks loaded successfully!")
-        except:
-            print("No tasks found, are you sure a tasklist.json exists?")
+        self.tasks = TaskStorage().load_tasks(self)
 
     def edit_task(self, task_id: str):
         if task_id not in self.tasks:
