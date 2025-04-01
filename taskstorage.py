895de097056e27f171572630d5d406e5ee9621b5
CarsonDSGrant <139571239+CarsonDSGrant@users.noreply.github.com>
2025-03-31 22:44:49 -0600

    Update taskstorage.py
    updated to match with the Singleton pattern

--- taskstorage.py
+++ taskstorage.py
@@ -1,30 +1,30 @@
 from task import Task
-from task_manager import TaskManager
 import json
 
 class TaskStorage():
     def save_tasks(self, tasks):
         tasks_dict = {}
         for task_id in tasks:
-            target_task = self.tasks[task_id]
+            target_task = tasks[task_id] 
             tasks_dict[task_id] = [target_task.title, target_task.description, target_task.due_date, target_task.priority, target_task.tags]
-        task_json = json.dumps(tasks_dict)
         with open("tasklist.json", 'w') as tasks_file:
-            tasks_file.writelines(task_json)
-    
+            json.dump(tasks_dict, tasks_file)
+        print("Tasks saved successfully!")
+
     def load_tasks(self, task_man):
         try:
             with open("tasklist.json", 'r') as tasks_file:
-                task_json = tasks_file.read()
-                tasks_dict = json.loads(task_json)
-                self.tasks = {}
-                for taskitem_id in tasks_dict.keys():
-                    added_task = Task(tasks_dict[taskitem_id][0])
-                    added_task.task_id = taskitem_id
-                    added_task.description = tasks_dict[taskitem_id][1]
-                    added_task.due_date = tasks_dict[taskitem_id][2]
-                    added_task.priority = tasks_dict[taskitem_id][3]
-                    added_task.tags = tasks_dict[taskitem_id][4]
-                    task_man.tasks[taskitem_id] = added_task
-        except:
-            raise FileNotFoundError
+                tasks_dict = json.load(tasks_file)
+                for task_id, task_data in tasks_dict.items():
+                    added_task = Task(task_data[0])
+                    added_task.task_id = task_id
+                    added_task.description = task_data[1]
+                    added_task.due_date = task_data[2]
+                    added_task.priority = task_data[3]
+                    added_task.tags = task_data[4]
+                    task_man.tasks[task_id] = added_task
+                print("Tasks loaded successfully!")
+                return task_man.tasks 
+        except FileNotFoundError:
+            print("No saved tasks found.")
+            return {}
