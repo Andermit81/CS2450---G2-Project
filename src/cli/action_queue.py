2b767cc3e44f5f9802d6df794debd71f2f638f4a
Noah-Blevins <noah.james.blevins@gmail.com>
2025-04-14 22:54:08 -0600

    Added undo/redo for add/delete

--- src/cli/action_queue.py
+++ src/cli/action_queue.py
@@ -8,62 +8,65 @@ class ActionQueue:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(ActionQueue, cls).__new__(cls)
-            cls._instance._undo_queue = []
-            cls._instance._redo_queue = []
-            cls._instance._task_man_instance = TaskManager()
-        return cls._instance 
-    
-    '''   
-        def __init__(self):
+            cls._undo_queue = []
+            cls._redo_queue = []
+            cls._task_man_instance = TaskManager()
+        return cls._instance   
+    '''
+    def __init__(self):
         self._undo_queue = []
         self._redo_queue = []
         self._task_man_instance = TaskManager()
     '''
+    
+    @classmethod    
+    def add_action(cls, restore_item, action: str):
+        ActionQueue._undo_queue.append([restore_item, action])
+        print("Adding action")
+      
+    @classmethod    
+    def undo_action(cls):
+        action_item = ActionQueue._undo_queue.pop()
+        ActionQueue._redo_queue.append(action_item)
         
-    def add_action(self, restore_item, action: str):
-        self._undo_queue.append([restore_item, action])
-        
-    def undo_action(self):
-        action_item = self._undo_queue.pop()
-        self._redo_queue.append(action_item)
-        
-        if len(self._undo_queue) == 0:
+        if len(ActionQueue._undo_queue) < 0:
             print("Nothing to undo")
             return
         
         if action_item[1] == "add":
-            self._task_man_instance.remove_task(action_item[0])
+            ActionQueue._task_man_instance.remove_task(action_item[0].task_id)
         
         elif action_item[1] == "delete":
-            self._task_man_instance.add_task(action_item[0])
+            ActionQueue._task_man_instance.add_task(action_item[0])
             
         elif action_item[1] == "edit":
-            self._redo_queue[-1][0] = self._task_man_instance.tasks[action_item[0].id]
-            self._task_man_instance.tasks[action_item[0].id] = action_item[0]
+            ActionQueue._redo_queue[-1][0] = ActionQueue._task_man_instance.tasks[action_item[0].task_id]
+            ActionQueue._task_man_instance.tasks[action_item[0].task_id] = action_item[0]
         
         elif action_item[1] == "sort":
-            self._redo_queue[-1][0] = self._task_man_instance.tasks
-            self._task_man_instance.tasks = action_item[0]
+            ActionQueue._redo_queue[-1][0] = ActionQueue._task_man_instance.tasks
+            ActionQueue._task_man_instance.tasks = action_item[0]
+    
+    @classmethod    
+    def redo_action(cls):
+        action_item = ActionQueue._redo_queue.pop()
+        ActionQueue._undo_queue.append(action_item)
         
-    def redo_action(self):
-        action_item = self._redo_queue.pop()
-        self._undo_queue.append(action_item)
-        
-        if len(self._redo_queue) == 0:
+        if len(ActionQueue._redo_queue) < 0:
             print("Nothing to redo")
             return
         
         if action_item[1] == "add":
-            self._task_man_instance.add_task(action_item[0])
+            ActionQueue._task_man_instance.add_task(action_item[0])
         
         elif action_item[1] == "delete":
-            self._task_man_instance.remove_task(action_item[0])
+            ActionQueue._task_man_instance.remove_task(action_item[0].task_id)
         
         elif action_item[1] == "edit":
-            self._undo_queue[-1][0] = self._task_man_instance.tasks[action_item[0].id]
-            self._task_man_instance.tasks[action_item[0].id] = action_item[0]
+            ActionQueue._undo_queue[-1][0] = ActionQueue._task_man_instance.tasks[action_item[0].task_id]
+            ActionQueue._task_man_instance.tasks[action_item[0].task_id] = action_item[0]
         
         elif action_item[1] == "sort":
-            self._undo_queue[-1][0] = self._task_man_instance.tasks
-            self._task_man_instance.tasks = action_item[0]
+            ActionQueue._undo_queue[-1][0] = ActionQueue._task_man_instance.tasks
+            ActionQueue._task_man_instance.tasks = action_item[0]
          
