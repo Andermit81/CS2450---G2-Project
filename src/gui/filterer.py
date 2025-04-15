c5329f58a8953a00384d21ccd56a76f5f52df141
CarsonDSGrant <139571239+CarsonDSGrant@users.noreply.github.com>
2025-04-14 20:31:40 -0600

    Update filterer.py
    updated documentation

--- src/gui/filterer.py
+++ src/gui/filterer.py
@@ -3,11 +3,13 @@ from ..cli.task import Task as task
 from ..cli.task_manager import TaskManager as task_manager
 
 class Filterer(ABC):
+    """Abstract base class for task filtering strategies"""
     @abstractmethod
     def filter(tasks: dict):
         pass
     
 class PriorityFilterer(Filterer):
+    """Strategy to filter by priority"""
     def filter(tasks: dict, priority: str) :
         for task in tasks.values():
             if task.priority == priority:
@@ -16,6 +18,7 @@ class PriorityFilterer(Filterer):
                 task.visible = False
 
 class TagFilterer(Filterer):
+    """Strategy to filter by tags"""
     def filter(tasks: dict, tag: str):
         for task in tasks.values():
             if tag in task.tags:
@@ -24,16 +27,19 @@ class TagFilterer(Filterer):
                 task.visible = False
 
 class ShowAllFilterer(Filterer):
+    """Strategy to filter nothing and show all tasks"""
     def filter(tasks: dict):
         for task in tasks.values():
             task.visible = True
     
 class DefaultFilterer(Filterer):
+    """Default filtering strategy"""
     def filter(tasks: dict):
         for task in tasks.values():
             task.visible = "Done" not in task.tags
             
 class CompleteFilterer(Filterer):
+    """Strategy to filter out tasks with the "Done" tag"""
     def filter(tasks: dict):
         for task in tasks.values():
             task.visible = "Done" in task.tags
